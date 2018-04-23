dcd69244309832d947839398380b2db9ff0ee709
Markus Petrykowski <markus.petrykowski@nexenio.com>
2018-04-23 17:47:04 +0200

    Add Flwo for TopBar

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,7 +1,12 @@
+// @flow
 import React from "react";
 import Container from "../Styled/Container";
 import Label from "../Styled/Text/Label";
 import { Link } from "react-router-dom";
+
+type PropsType = {
+  dark: boolean
+};
 
 const TopContainer = Container.extend`
   position: absolute;
@@ -10,10 +15,12 @@ const TopContainer = Container.extend`
   z-index: 10;
   padding: 1rem;
   width: calc(100% - 2rem);
-  background: ${props => (props.dark ? "#4079d6e0" : "none")};
+  background: ${(props: PropsType) => (props.dark ? "#4079d6e0" : "none")};
 `;
 
-export default class TopBar extends React.Component {
+export default class TopBar extends React.Component<{}, PropsType, {}> {
+  static defaultProps = {};
+  state = {};
   render() {
     return (
       <TopContainer dark={this.props.dark}>
